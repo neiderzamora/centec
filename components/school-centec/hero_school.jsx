620540e5650d889ab27ea893bd78789ab04d91e4
neiderzamora <neiderzamoramoreno@gmail.com>
2024-05-28 23:15:40 -0500

    add video school centec

--- components/school-centec/hero_school.jsx
+++ components/school-centec/hero_school.jsx
@@ -1,14 +1,14 @@
 export default function HeroSchool() {
   return (
     <section id="HeroSchool" className="overflow-hidden relative -z-40 bg-white">
-      <div className="mx-auto max-w-full">
+      <div className="mx-auto max-w-full h-auto">
         <video
           autoPlay
           loop
           muted
-          className="lg:w-full h-full lg:h-[40rem] object-cover"
+          className="h-full lg:h-[48rem] object-cover"
         >
-          <source src="/home_video.mp4" type="video/mp4" />
+          <source src="/school_centec.mp4" type="video/mp4" />
         </video>
       </div>
     </section>
